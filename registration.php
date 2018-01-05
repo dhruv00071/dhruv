e05087753073b60fadf87a8654009c6104ffbdbd
Swapnil Jaiswal <jaiswalswapnil1308@gmail.com>
2018-01-05 12:30:40 +0530

    Search bar changed

--- registration.php
+++ registration.php
@@ -84,7 +84,7 @@
             </div>
           </li>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2"  type="search" placeholder="Search" aria-label="Search" style="width:1200px; margin: 0 auto">
+            <input class="form-control mr-sm-2"  type="search" placeholder="Search" aria-label="Search" style="width:500px; margin: 0 auto">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </ul>
