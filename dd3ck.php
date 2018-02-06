d608d6aa43e2e5c0788f26d43f8d7649b4ed7d68
dhruv0007 <dprobo5@gmail.com>
2018-02-06 17:33:26 +0530

    Add files via upload

---
+++ dd3ck.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title>Product Confirmation</title>
+</head>
+<body>
+<?php
+include 'connect.php';
+if (isset($_POST['upload']))
+{
+	$target = "pimages/".basename($_FILES['image']['name']);
+	$image = $_FILES['image']['name'];
+	$cat=$_POST['cat'];
+	$subcat=$_POST['subcat'];
+	$subcat3=$_POST['cat3'];
+	$des =  mysqli_real_escape_string ($con,$_POST['des']);
+	$price=$_POST['price'];
+	$model=mysqli_real_escape_string ($con,$_POST['model']);
+	$quantity=$_POST['quantity'];
+	$sql = "INSERT INTO product_details(category,type,brand,price,model_no,image,quantity,des) VALUES ('$cat','$subcat','$subcat3','$price','$model','$image','$quantity','$des')";
+	if (move_uploaded_file($_FILES['image']['tmp_name'], $target))
+	{
+		echo '<script>alert("Product inserted into Database")</script>';
+		echo 'Product succesfully added to your account.';
+		echo 'Please wait for the confirmation from our backend team!';
+	}
+	else
+	{
+		echo"Failed to upload your product";
+    }
+}
+?>
+</body>
+</html>
